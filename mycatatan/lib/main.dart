ba29c2585fc685045996b78dad6c375c9c280bba
Isra23343045 <alfattah.isra@gmail.com>
2026-03-08 12:08:13 +0700

    Chapter 25 : Migrating to Auth Service

--- mycatatan/lib/main.dart
+++ mycatatan/lib/main.dart
@@ -1,11 +1,8 @@
-import 'dart:developer';
-
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mycatatan/constants/routes.dart';
-import 'package:mycatatan/firebase_options.dart';
+import 'package:mycatatan/services/auth/auth_service.dart';
 import 'package:mycatatan/views/login_view.dart';
+import 'package:mycatatan/views/notes_view.dart';
 import 'package:mycatatan/views/register_view.dart';
 import 'package:mycatatan/views/verify_email_view.dart';
 
@@ -34,15 +31,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      ),
+      future: AuthService.firebase().initialize(), 
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            final user = FirebaseAuth.instance.currentUser;
+            final user = AuthService.firebase().currentUser;
             if (user != null) {
-              if (user.emailVerified) {
+              if (user.isEmailVerified) {
                 return const NoteView();
               } else {
                 return const VerifyEmailView();
@@ -53,79 +48,10 @@ class HomePage extends StatelessWidget {
           default:
             return const CircularProgressIndicator();
         }
-        return const CircularProgressIndicator();
       },
     );
   }
 }
 
-enum MenuAction { logout }
 
-class NoteView extends StatefulWidget {
-  const NoteView({super.key});
 
-  @override
-  State<NoteView> createState() => _NoteViewState();
-}
-
-class _NoteViewState extends State<NoteView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Main UI"),
-        actions: [
-          PopupMenuButton<MenuAction>(
-            onSelected: (value) async {
-              switch (value) {
-                case MenuAction.logout:
-                  final shouldLogout = await showLogOutDialog(context);
-                  if (shouldLogout) {
-                    await FirebaseAuth.instance.signOut();
-                    Navigator.of(
-                      context,
-                    ).pushNamedAndRemoveUntil(loginRoute, (_) => false);
-                  }
-                  break;
-              }
-            },
-            itemBuilder: (context) {
-              return [
-                const PopupMenuItem<MenuAction>(
-                  value: MenuAction.logout,
-                  child: Text("Logout"),
-                ),
-              ];
-            },
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-Future<bool> showLogOutDialog(BuildContext context) {
-  return showDialog<bool>(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text('Sign out'),
-        content: const Text('Are you sure you want to sign out?'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(false);
-            },
-            child: const Text('Cancel'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(true);
-            },
-            child: const Text('Log Out'),
-          ),
-        ],
-      );
-    },
-  ).then((value) => value ?? false);
-}
--- mycatatan/lib/views/login_view.dart
+++ mycatatan/lib/views/login_view.dart
@@ -1,8 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
 import 'package:mycatatan/constants/routes.dart';
+import 'package:mycatatan/services/auth/auth_exceptions.dart';
+import 'package:mycatatan/services/auth/auth_service.dart';
 import 'package:mycatatan/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -56,11 +57,11 @@ class _LoginViewState extends State<LoginView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                await FirebaseAuth.instance.signInWithEmailAndPassword(
+                await AuthService.firebase().login(
                   email: email, 
                   password: password);
-                final user = FirebaseAuth.instance.currentUser;
-                if(user?.emailVerified ?? false){
+                final user = AuthService.firebase().currentUser;
+                if(user?.isEmailVerified ?? false){
                     Navigator
                     .of(context)
                     .pushNamedAndRemoveUntil(VerifyEmailViewRoute, (_) => false);
@@ -68,16 +69,12 @@ class _LoginViewState extends State<LoginView> {
 
                 }
                     
-              } on FirebaseAuthException catch (e) {
-                if (e.code == 'user - not - found') {
-                  await showErrorDialog(context, 'User Not Found',);
-                } else if (e.code == 'wrong-password') {
-                  await showErrorDialog(context, 'Wrong Password',);
-                } else {
-                  await showErrorDialog(context, 'Error : ${e.code}',);
-                }
-              } catch (e) {
-                await showErrorDialog(context, 'Error : ${e.toString()}',);
+              } on UserNotFoundAuthException{
+                await showErrorDialog(context, 'User Not Found',);
+              } on WrongPasswordAuthException{
+                await showErrorDialog(context, 'Wrong Password',);
+              } on GenericAuthException{
+                await showErrorDialog(context, 'Authentication Error',);
               }
             },
             child: const Text('Login'),
