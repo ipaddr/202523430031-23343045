30d717cbc5b5f2fc43be91db37ee4d5af2ca7fa5
Isra23343045 <alfattah.isra@gmail.com>
2026-04-06 21:15:07 +0700

    Chapter 35 - Protecting NotesService with Current User

--- mycatatan/lib/main.dart
+++ mycatatan/lib/main.dart
@@ -20,7 +20,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
